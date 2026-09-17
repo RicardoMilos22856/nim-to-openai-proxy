f48d35d4e83c2a9acadc4c34d757555325f0eeee
RicardoMilos22856 <89120657+RicardoMilos22856@users.noreply.github.com>
2026-09-18 01:03:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -499,7 +499,7 @@ async function callWithFallback(baseRequest, models, enableThinking, clientReaso
             httpAgent,
             httpsAgent,
             responseType: baseRequest.stream ? 'stream' : 'json',
-            timeout: 120000
+            timeout: 180000
           }
         );
 
